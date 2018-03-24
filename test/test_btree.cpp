fd154e212fc7feeab4d727abe0d6d76ba5cb842d
Harikrishnan <harikrishnan.prabakaran@zohocorp.com>
2018-03-24 10:22:48 +0530

    Fix bug in BtreeMixedTest

--- test/test_btree.cpp
+++ test/test_btree.cpp
@@ -61,6 +61,126 @@ TEST_CASE("BtreeBasicTest", "[bwtree]")
 }
 
 
+TEST_CASE("BtreeMixedTest", "[btree]")
+{
+	auto key_size = [](const void *key, void *extra_arg)
+					{
+						return static_cast<int>(sizeof(long));
+					};
+	auto compare_key = [] (const void *a1, const void *a2, void *extra_arg)
+					   {
+						   return static_cast<int>(*reinterpret_cast<const long *>(a1) -
+												   *reinterpret_cast<const long *>(a2));
+					   };
+	auto get_void_ptr = [](long v)
+						{
+							return reinterpret_cast<const void *>(v);
+						};
+	auto convert_to_void_ptr = [](long *v)
+							   {
+								   return reinterpret_cast<void **>(v);
+							   };
+	auto get_key_ptr = [](const int *v)
+					   {
+						   return reinterpret_cast<const void *>(v);
+					   };
+
+	btree_key_val_info_t kv_info = { compare_key, key_size, NULL };
+
+	int		btree_pagesize = 128;
+	int		num_operations = 1024 * 1024;
+	int		cardinality	   = num_operations * 0.1;
+	btree_t btree		   = btree_create(btree_pagesize, &kv_info);
+
+	constexpr auto INSERT_OP = 1;
+	constexpr auto LOOKUP_OP = 2;
+	constexpr auto DELETE_OP = 3;
+
+	std::random_device r;
+	std::seed_seq	   seed2{ r(), r(), r(), r(), r(), r(), r(), r() };
+	std::mt19937	   e2(seed2);
+
+	std::uniform_int_distribution<int>				   key_dist{ 1, cardinality };
+	std::uniform_int_distribution<decltype(INSERT_OP)> op_dist{ INSERT_OP, DELETE_OP };
+
+	std::unordered_map<int, long> key_values;
+
+	for (int i = 0; i < num_operations; i++)
+	{
+		const auto key = key_dist(e2);
+		const auto op  = op_dist(e2);
+		long	   val = 0;
+
+		switch (op)
+		{
+			case INSERT_OP:
+			{
+				if (key_values.count(key))
+				{
+					REQUIRE(btree_find(btree, get_key_ptr(&key),
+									   convert_to_void_ptr(&val)) == true);
+					REQUIRE(btree_delete(btree, get_key_ptr(&key),
+										 convert_to_void_ptr(&val)) == true);
+					REQUIRE(val == key_values[key]);
+					key_values.erase(key);
+				}
+
+				val = key * key_dist(e2);
+
+				REQUIRE(btree_insert(btree, get_key_ptr(&key), get_void_ptr(val)) == true);
+				key_values[key] = reinterpret_cast<long>(val);
+				break;
+			}
+
+			case LOOKUP_OP:
+			{
+				if (key_values.count(key))
+				{
+					REQUIRE(btree_find(btree, get_key_ptr(&key),
+									   convert_to_void_ptr(&val)) == true);
+					REQUIRE(reinterpret_cast<long>(val) == key_values[key]);
+				}
+				else
+				{
+					REQUIRE(btree_find(btree, get_key_ptr(&key),
+									   convert_to_void_ptr(&val)) == false);
+				}
+				break;
+			}
+
+			case DELETE_OP:
+			{
+				if (key_values.count(key))
+				{
+					REQUIRE(btree_delete(btree, get_key_ptr(&key),
+										 convert_to_void_ptr(&val)) == true);
+					REQUIRE(reinterpret_cast<long>(val) == key_values[key]);
+					key_values.erase(key);
+				}
+				else
+				{
+					REQUIRE(btree_delete(btree, get_key_ptr(&key),
+										 convert_to_void_ptr(&val)) == false);
+				}
+				break;
+			}
+
+			default:
+				continue;
+		}
+	}
+
+	for (const auto &kv: key_values)
+	{
+		void *val;
+
+		REQUIRE(btree_delete(btree, &kv.first, &val) == true);
+		REQUIRE(reinterpret_cast<long>(val) == kv.second);
+		REQUIRE(btree_find(btree, &kv.first, &val) == false);
+	}
+}
+
+
 TEST_CASE("BtreeTestString", "[btree]")
 {
 	auto key_size = [](const void *key, void *extra_arg)
@@ -106,113 +226,5 @@ TEST_CASE("BtreeTestString", "[btree]")
 
 		REQUIRE(btree_delete(btree, kv.first, &val) == true);
 		REQUIRE(reinterpret_cast<long>(val) == kv.second);
-	}
-}
-
-
-TEST_CASE("BtreeMixedTest", "[btree]")
-{
-	auto key_size = [](const void *key, void *extra_arg)
-					{
-						return static_cast<int>(sizeof(long));
-					};
-	auto compare_key = [] (const void *a1, const void *a2, void *extra_arg)
-					   {
-						   return static_cast<int>(*reinterpret_cast<const long *>(a1) -
-												   *reinterpret_cast<const long *>(a2));
-					   };
-	auto get_ptr = [](int v)
-				   {
-					   return reinterpret_cast<const void *>(v);
-				   };
-	auto get_key_ptr = [](const int *v)
-					   {
-						   return reinterpret_cast<const void *>(v);
-					   };
-
-
-	btree_key_val_info_t kv_info = { compare_key, key_size, NULL };
-
-	int		btree_pagesize = 128;
-	int		num_operations = 1024 * 1024;
-	int		cardinality	   = num_operations * 0.3;
-	btree_t btree		   = btree_create(btree_pagesize, &kv_info);
-
-	constexpr auto INSERT_OP = 1;
-	constexpr auto LOOKUP_OP = 2;
-	constexpr auto DELETE_OP = 3;
-
-	std::random_device r;
-	std::seed_seq	   seed2{ r(), r(), r(), r(), r(), r(), r(), r() };
-	std::mt19937	   e2(seed2);
-
-	std::uniform_int_distribution<int>				   key_dist{ 1, cardinality };
-	std::uniform_int_distribution<decltype(INSERT_OP)> op_dist{ INSERT_OP, DELETE_OP };
-
-	std::unordered_map<int, int> key_values;
-
-	for (int i = 0; i < num_operations; i++)
-	{
-		const auto key	= key_dist(e2);
-		const auto op	= op_dist(e2);
-		void	   *val = reinterpret_cast<void *>(key * key_dist(e2));
-
-		switch (op)
-		{
-			case INSERT_OP:
-			{
-				if (key_values.count(key))
-				{
-					REQUIRE(btree_find(btree, get_key_ptr(&key), &val) == true);
-					REQUIRE(btree_delete(btree, get_key_ptr(&key), &val) == true);
-					key_values.erase(key);
-				}
-
-				REQUIRE(btree_insert(btree, get_key_ptr(&key), val) == true);
-				key_values[key] = reinterpret_cast<long>(val);
-				break;
-			}
-
-			case LOOKUP_OP:
-			{
-				if (key_values.count(key))
-				{
-					REQUIRE(btree_find(btree, get_key_ptr(&key), &val) == true);
-					REQUIRE(reinterpret_cast<long>(val) == key_values[key]);
-				}
-				else
-				{
-					REQUIRE(btree_find(btree, get_key_ptr(&key), &val) == false);
-				}
-				break;
-			}
-
-			case DELETE_OP:
-			{
-				if (key_values.count(key))
-				{
-					REQUIRE(btree_delete(btree, get_key_ptr(&key), &val) == true);
-					REQUIRE(reinterpret_cast<long>(val) == key_values[key]);
-					key_values.erase(key);
-				}
-				else
-				{
-					REQUIRE(btree_delete(btree, get_key_ptr(&key), &val) == false);
-				}
-				break;
-			}
-
-			default:
-				continue;
-		}
-	}
-
-	for (const auto &kv: key_values)
-	{
-		void *val;
-
-		REQUIRE(btree_delete(btree, &kv.first, &val) == true);
-		REQUIRE(reinterpret_cast<long>(val) == kv.second);
-		REQUIRE(btree_find(btree, &kv.first, &val) == false);
 	}
 }
