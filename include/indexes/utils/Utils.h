0b3ef26a99679223f8241cdb4b6a2580ff70592a
Harikrishnan <harikrishnan.prabakaran@zohocorp.com>
2019-05-13 17:38:11 +0530

    Fix error due to CHAR_BIT usage

--- include/indexes/utils/Utils.h
+++ include/indexes/utils/Utils.h
@@ -59,6 +59,11 @@ static inline int __builtin_clzl(unsigned long long x) {
 #define _RESTRICT __restrict
 #endif
 
+#ifndef CHAR_BIT
+// Lets hope this is true
+#define CHAR_BIT 8
+#endif
+
 namespace indexes::utils {
 static inline int leading_zeroes(uint64_t val) { return __builtin_clzl(val); }
 static inline int leading_zeroes(uint32_t val) { return __builtin_clz(val); }
